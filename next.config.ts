4006d70730b07698da48aa11a68799c9920445a8
faizdev007 <faizdev007@gmail.com>
2025-08-08 12:13:35 +0530

    update redirection

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint during build
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/", // Change to your desired landing page
+        permanent: true,  // 308 redirect, cached by browsers & search engines
+      },
+      {
+        source: "/hire",
+        destination: "/", // Or /404 if you want a not-found page
+        permanent: true,
+      }
+    ];
+  },
 };
 
 export default nextConfig;
