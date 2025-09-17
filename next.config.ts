625e1ebfb3900a248d2532fb435c5115149cb66d
faizdev007 <faizdev007@gmail.com>
2025-09-17 15:27:06 +0530

    update config

--- next.config.ts
+++ next.config.ts
@@ -56,16 +56,16 @@ const nextConfig: NextConfig = {
   },
   async redirects() {
     return [
-      // {
-      //   source: "/blog",
-      //   destination: "/", // Change to your desired landing page
-      //   permanent: true,  // 308 redirect, cached by browsers & search engines
-      // },
-      // {
-      //   source: "/hire",
-      //   destination: "/", // Or /404 if you want a not-found page
-      //   permanent: true,
-      // },
+      {
+        source: "/blog",
+        destination: "/", // Change to your desired landing page
+        permanent: true,  // 308 redirect, cached by browsers & search engines
+      },
+      {
+        source: "/hire",
+        destination: "/", // Or /404 if you want a not-found page
+        permanent: true,
+      },
       // {
       //   source: "/hire/:path*",
       //   destination: "/", // Or /404 if you want a not-found page
