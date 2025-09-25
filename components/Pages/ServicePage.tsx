b492c34730989622860d8ca4a87f9eb49066c989
faizdev007 <faizdev007@gmail.com>
2025-09-25 16:03:15 +0530

    fix smooth scrolling issue

--- components/Pages/ServicePage.tsx
+++ components/Pages/ServicePage.tsx
@@ -28,43 +28,29 @@ export default function ServicePage(pageData:any) {
       <HeroSection HeroData={pageData?.pageData?.HeroSection}/>
       <ClientScroll/>
       <ImageSteps StepsBlock={pageData?.pageData?.stepCard}/>
-      <div id="SWS" className="overflow-y-scroll scroll-mt-18 scroll-smooth">
+      <div id="SWS" className="scroll-mt-18 scroll-smooth">
         <ServicessArea ServicessArea={pageData?.pageData?.hoverCards?.group2}/>
       </div>
       <VACTA CTA={pageData?.pageData?.cta?.one}/>
       <SkillsVA Skills={pageData?.pageData?.cursorBlock}/>
-      <div id="SS" className="overflow-y-scroll scroll-mt-18 scroll-smooth bg-oveblue/10">
+      <div id="SS" className="scroll-mt-18 scroll-smooth bg-oveblue/10">
         <ClientSuccessStories ClientStories={pageData?.pageData?.clientStories}/>
         <ClientReview Testimonials={pageData?.pageData?.testimonial}/>
       </div>
       <VACTA CTA={pageData?.pageData?.cta?.two}/>
-      <div id="WWS" className="overflow-y-scroll scroll-mt-18 scroll-smooth">
+      <div id="WWS" className="scroll-mt-18 scroll-smooth">
         <ServiceWhyChooseUs WhyChooseUs={pageData?.pageData?.whyChoose}/>
       </div>
       <ImagineTask Information={pageData?.pageData?.infoCards}/>
-      <div id="IWS" className="overflow-y-scroll scroll-mt-18 scroll-smooth">
+      <div id="IWS" className="scroll-mt-18 scroll-smooth">
         <RoleWeHired RoleBaseHired={pageData?.pageData?.hoverCards?.group1}/>
       </div>
       <WhyYourShouldChoose/>
       <VAWork StoryBlock={pageData?.pageData?.storyBlock}/>
       <VACTA2 CTA2={pageData?.pageData?.cta?.three}/>
-      <div id="FAQ" className="overflow-y-scroll scroll-mt-18 scroll-smooth">
+      <div id="FAQ" className="scroll-mt-18 scroll-smooth">
         <FaqSection FAQ={pageData?.pageData?.faq}/>
       </div>
-      
-      {/* Modal */}
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <h2 className="text-xl font-semibold mb-4">Hello from Modal 🎉</h2>
-        <p className="text-gray-600 dark:text-gray-300 mb-6">
-          This is a reusable modal component in Next.js.
-        </p>
-        <button
-          onClick={() => setIsOpen(false)}
-          className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700"
-        >
-          Close
-        </button>
-      </Modal>
     </div>
   );
 }
