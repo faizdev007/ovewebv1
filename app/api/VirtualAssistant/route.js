5891a32e39a6157d57afa88a3f08412390dd56fd
faizdev007 <faizdev007@gmail.com>
2025-09-28 00:55:24 +0530

    add more emails id

--- app/api/VirtualAssistant/route.js
+++ app/api/VirtualAssistant/route.js
@@ -20,7 +20,7 @@ export async function POST(request) {
     try {
         await transporter.sendMail({
         from: `"OVE" <faizdev007@gmail.com>`,
-        to: ['faizdev007@gmail.com'], // Your receiving email address ,'Nakul@optimalvirtualemployee.com','ronnie@optimalvirtualemployee.com','dshah@optimalvirtualemployee.com'
+        to: ['faizdev007@gmail.com','Nakul@optimalvirtualemployee.com','ronnie@optimalvirtualemployee.com','dshah@optimalvirtualemployee.com'], // Your receiving email address
         subject: 'New Form Submission',
         html: `<!DOCTYPE html>
                 <html>
